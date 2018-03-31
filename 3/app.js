1f728323abd50e56c31712841afc387d76c689af
Vuk Samardžić <samardzic.vuk@gmail.com>
2018-03-31 14:00:00 +0100

    challenge 3 complete

---
+++ 3/app.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+
+let buf = fs.readFile(process.argv[2], 'utf8', (err, data) => {
+    if (err) {
+        return console.log(err)
+    }
+    console.log(data.split('\n').length - 1);
+});
