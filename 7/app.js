dcb3d0c9eb3d03847a208f9ff54917dc0cc031a4
Vuk Samardžić <samardzic.vuk@gmail.com>
2018-04-04 14:00:00 +0100

    challenge 7 complete

---
+++ 7/app.js
@@ -0,0 +1,16 @@
+const http = require('http');
+
+http.get(process.argv[2], function (result) {
+    let data = '';
+    result.setEncoding('utf8');
+    result.on('error', function (err) {
+        console.error(err);
+    });
+    result.on('data', function (chunk) {
+        data += chunk;
+    });
+    result.on('end', function () {
+        console.log(data.length);
+        console.log(data);
+    });
+});
